e5cc10f0c5b9990537ced083b8b0f097f41279be
Tunde Osborne <osbornetunde@gmail.com>
2018-09-28 21:33:39 +0100

    first step

---
+++ journalApp.py
@@ -0,0 +1,46 @@
+def main():
+    print_header()
+    run_event_loop()
+
+
+def print_header():
+    print("---------------------------")
+    print("       Journal App")
+    print("---------------------------")
+
+
+def run_event_loop():
+
+    print("What do you want to do with your journal?")
+
+    cmd = None
+    journal_data = []
+
+    while cmd != 'x':
+
+        cmd = input("[L]ist entries, [A]dd an entry, E[x]it: ")
+        cmd = cmd.lower().strip()
+
+        if cmd == "l":
+            list_entries(journal_data)
+        elif cmd == "a":
+            add_entry(journal_data)
+        elif cmd != 'x':
+            print("Sorry, we don't understand '{}'.".format(cmd))
+
+    print("Done, goodbye")
+
+
+def list_entries(data):
+    print("Your journal entries:")
+    entries = reversed(data)
+    for entry in entries:
+        print(entry)
+
+
+def add_entry(data):
+    text = input("Type your entry, <enter> to exit: ")
+    data.append(text)
+
+
+main()
